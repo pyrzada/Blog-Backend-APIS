85e5c4244e459adcf3b70e0e15fa05061e759c7c
pyrzada <awahabpirzada@gmail.com>
2023-04-20 16:54:54 -0400

    removed unused imports

--- src/main/java/com/behloleaqil/blog/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/behloleaqil/blog/exceptions/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.behloleaqil.blog.exceptions;
 import com.behloleaqil.blog.payloads.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
