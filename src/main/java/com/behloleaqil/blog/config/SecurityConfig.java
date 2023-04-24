ae62ecb31f4906da365f5483266228d58c432772
pyrzada <awahabpirzada@gmail.com>
2023-04-23 23:26:59 -0400

    implementing basic security for APIS but i'm having error on WebSecurityAdapter

---
+++ src/main/java/com/behloleaqil/blog/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.behloleaqil.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends SecurityConfigurerAdapter {
+
+    @Override
+    protected void securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
